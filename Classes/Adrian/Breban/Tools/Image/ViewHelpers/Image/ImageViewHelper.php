f0043819c32858a9b4a400f1e8035d64290cf6ff
adibreban <adibreban@yahoo.com>
2013-09-20 20:15:19 +0300

    first changes

--- Classes/Adrian/Breban/Tools/Image/ViewHelpers/Image/ImageViewHelper.php
+++ Classes/Adrian/Breban/Tools/Image/ViewHelpers/Image/ImageViewHelper.php
@@ -5,10 +5,71 @@ use TYPO3\Flow\Annotations as Flow;
 //use TYPO3\Media\Domain\Model\ImageInterface;
 use Adrian\Breban\Tools\Image\Domain\Model\ImageInterface;
 
-//class ImageViewHelper extends \TYPO3\Media\ViewHelpers\ImageViewHelper{
-class ImageViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper {
 
-	public function render(\Adrian\Breban\Tools\Image\Domain\Model\ImageInterface $image = NULL, $maximumWidth = NULL, $maximumHeight = NULL, $allowCropping = FALSE, $allowUpScaling = FALSE, $alt=false){
+ /**
+ * Renders an <img> HTML tag from a given TYPO3.Media's Image instance
+ *
+ * = Examples =
+ *
+ * <code title="Rendering an image as-is">
+ * <m:image image="{imageObject}" alt="a sample image without scaling" />
+ * </code>
+ * <output>
+ * (depending on the image, no scaling applied)
+ * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="120" height="180" alt="a sample image without scaling" />
+ * </output>
+ *
+ *
+ * <code title="Rendering an image with scaling at a given width only">
+ * <m:image image="{imageObject}" maximumWidth="80" alt="sample" />
+ * </code>
+ * <output>
+ * (depending on the image; scaled down to a maximum width of 80 pixels, keeping the aspect ratio)
+ * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="80" height="120" alt="sample" />
+ * </output>
+ *
+ *
+ * <code title="Rendering an image with scaling at given width and height, keeping aspect ratio">
+ * <m:image image="{imageObject}" maximumWidth="80" maximumHeight="80" alt="sample" />
+ * </code>
+ * <output>
+ * (depending on the image; scaled down to a maximum width and height of 80 pixels, keeping the aspect ratio)
+ * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="53" height="80" alt="sample" />
+ * </output>
+ *
+ *
+ * <code title="Rendering an image with crop-scaling at given width and height">
+ * <m:image image="{imageObject}" maximumWidth="80" maximumHeight="80" allowCropping="true" alt="sample" />
+ * </code>
+ * <output>
+ * (depending on the image; scaled down to a width and height of 80 pixels, possibly changing aspect ratio)
+ * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="80" height="80" alt="sample" />
+ * </output>
+ *
+ * <code title="Rendering an image with allowed up-scaling at given width and height">
+ * <m:image image="{imageObject}" maximumWidth="5000" allowUpScaling="true" alt="sample" />
+ * </code>
+ * <output>
+ * (depending on the image; scaled up or down to a width 5000 pixels, keeping aspect ratio)
+ * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="80" height="80" alt="sample" />
+ * </output>
+ *
+ */
+ 
+class ImageViewHelper extends \TYPO3\Media\ViewHelpers\ImageViewHelper{
+//class ImageViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper {
+	/**
+	 * Renders an HTML tag from a given image.
+	 *
+	 * @param Adrian\Breban\Tools\Image\Domain\Model\ImageInterface $image If specified, this resource object is used instead of the path and package information
+	 * @param integer $maximumWidth Desired maximum height of the image
+	 * @param integer $maximumHeight Desired maximum width of the image
+	 * @param boolean $allowCropping Whether the image should be cropped if the given sizes would hurt the aspect ratio
+	 * @param boolean $allowUpScaling Whether the resulting image size might exceed the size of the original image
+	 *
+	 * @return string an <img...> html tag
+	 */
+	public function render(\Adrian\Breban\Tools\Image\Domain\Model\ImageInterface $image = NULL, $maximumWidth = NULL, $maximumHeight = NULL, $allowCropping = FALSE, $allowUpScaling = FALSE ){
 		$watermark = 'adi';
 		if($watermark)return $watermark;
 		else return 'adi';
